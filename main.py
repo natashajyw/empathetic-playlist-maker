80026ddc38dbf911863217408643ed605fddf2f3
natashajyw <wu.tasha88@gmail.com>
2023-10-27 19:05:51 -0700

    got hume api to work

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from hume import HumeStreamClient
+from hume.models.config import LanguageConfig
+
+samples = [
+    "Mary had a little lamb,",
+    "Its fleece was white as snow."
+    "Everywhere the child went,"
+    "The little lamb was sure to go."
+]
+
+async def main():
+    client = HumeStreamClient("7kBEBKHVRNGmYD4OQfIusARM8g40nvEGf5cXpUiAeohJZhCE")
+    config = LanguageConfig()
+    async with client.connect([config]) as socket:
+        for sample in samples:
+            result = await socket.send_text(sample)
+            emotions = result["language"]["predictions"][0]["emotions"]
+            print(emotions)
+
+asyncio.run(main())
