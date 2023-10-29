4db16302878466fdfcf45d5f67a541eb0bcdb890
natashajyw <wu.tasha88@gmail.com>
2023-10-28 19:33:27 -0700

    cleaned up emotions gathered from hume a little and was added music attributes

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import asyncio
 
+from enum import Enum
 from hume import HumeStreamClient
 from hume.models.config import LanguageConfig
 
 samples = [
-    "This is unbelieveable! I really want to have pizza right now."
+    "Today is a great day. I absolutely love the weather we have and I can't wait to get started with work today."
 ]
+
+class MusAttribute(Enum):
+    ENERGY = 0
+    LIVENESS = 1
+    LOUDNESS = 2
+    TEMPO = 3
 
 async def main():
     client = HumeStreamClient("7kBEBKHVRNGmYD4OQfIusARM8g40nvEGf5cXpUiAeohJZhCE")
@@ -18,6 +25,40 @@ async def main():
 
 async def function():
     list = await main()
-    print(list)
+    values_to_keep = ['Anger', 'Anxiety', 'Calmness', 'Distress', 'Ecstasy', 'Enthusiasm', 'Excitement', 'Joy', 'Sadness']
+
+    emotion_list = [item for item in list if item['name'] in values_to_keep]
+
+    emotion_list = sorted(emotion_list, key=lambda x: x['score'], reverse=True)
+    emotion_list = emotion_list[:3]
+
+    for item in emotion_list:
+        print(item)
+
+    attribute_list = [[0.0 for i in range(4)] for j in range(3)]
+
+    for i in range(3):
+        if emotion_list[i]['name'] == 'Anger' or emotion_list[i]['name'] == 'Distress' or emotion_list[i]['name'] == 'Anxiety' or emotion_list[i]['name'] == 'Calmness':
+            attribute_list[i][MusAttribute['ENERGY'].value] = 0.3
+            attribute_list[i][MusAttribute['LIVENESS'].value] = 0.3
+            attribute_list[i][MusAttribute['LOUDNESS'].value] = 0.4
+            attribute_list[i][MusAttribute['TEMPO'].value] = 60
+        elif emotion_list[i]['name'] == 'Ecstasy' or emotion_list[i]['name'] == 'Joy' or emotion_list[i]['name'] == 'Excitement' or emotion_list[i]['name'] == 'Enthusiasm':
+            attribute_list[i][MusAttribute['ENERGY'].value] = 0.8
+            attribute_list[i][MusAttribute['LIVENESS'].value] = 0.8
+            attribute_list[i][MusAttribute['LOUDNESS'].value] = 0.8
+            attribute_list[i][MusAttribute['TEMPO'].value] = 150
+        elif emotion_list[i]['name'] == 'Sadness':
+            attribute_list[i][MusAttribute['ENERGY'].value] = 0.1
+            attribute_list[i][MusAttribute['LIVENESS'].value] = 0.2
+            attribute_list[i][MusAttribute['LOUDNESS'].value] = 0.5
+            attribute_list[i][MusAttribute['TEMPO'].value] = 70
+
+    for item in attribute_list:
+        print(item)
 
 asyncio.run(function())
+
+
+
+
