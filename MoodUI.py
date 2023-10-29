74263bb4ed92e6b6263b6215475c7f7466e1a38f
Bala Vazrala <balavazrala@Balas-MacBook-Pro-2.local>
2023-10-28 23:59:47 -0700

    spotify button added

--- MoodUI.py
+++ MoodUI.py
@@ -2,7 +2,7 @@ import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
 
-st.set_page_config(page_title = "MoodUI", layout = "wide")
+st.set_page_config(page_title="MoodUI", layout="wide")
 
 # function to read the URL for the animation
 def load_lottieurl(url):
@@ -10,42 +10,31 @@ def load_lottieurl(url):
     if r.status_code != 200:
         return None
     return r.json()
-    
+
 #----------LOAD ASSETS------------
-# URL for the animation
 lottie_coding = load_lottieurl("https://lottie.host/f7e1e0d6-3005-4c3b-86ec-dfce25385f01/sPjyphcf6W.json")
 
 #----------FUNCTIONALITY----------
-# Title/Description/Animation
 with st.container():
     left_column, right_column = st.columns(2)
     with left_column:
-        # Import the Comfortaa font from Google Fonts
         st.markdown("""
             <style>
                 @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap');
             </style>
             """, unsafe_allow_html=True)
 
-        # Apply the Comfortaa font to your title using inline CSS
         st.markdown("""
         <h1 style="font-family: 'Comfortaa';"> MoodTune </h1>
         """, unsafe_allow_html=True)
         st.write(" ")
         st.write("Hi, welcome to MoodTune. MoodTune is a music recommendation AI. Use MoodTune to find songs that relate to how you're feeling. Enjoy!")
     with right_column:
-        st_lottie(lottie_coding, height = 200, key = "coding")
+        st_lottie(lottie_coding, height=200, key="coding")
     st.write("---")
-# User Input
-#  Custom CSS to center the textbox and style it
+
 st.markdown("""
 <style>
-    .centered {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 70vh;
-    }
     .stTextInput>div>div>input {
         font-size: 16px;
         padding: 10px 15px;
@@ -55,26 +44,17 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-# Center the textbox using new layout primitives
 cols = st.columns([1, 2, 1])
 with cols[1]:
-    user_input = st.text_input("How are you feeling today? Describe your mood in a complete sentence")
-
-# For formatting
-st.write(" ")
-st.write(" ")
-st.write(" ")
-st.write(" ")
-st.write(" ")
-st.write(" ")
-st.write(" ")
+    user_input = st.text_input("How are you feeling today? Describe your mood in a complete sentence.")
 st.write(" ")
 
-# Methodology/Credits
-with st.container():
-        st.header("Methodology")
-        st.write("""
-        1. Technologies used for UI: Python; StreamLit library and CSS.
-        2. Hume.ai API used to apply concepts of Sentiment Analysis.
-        3. Spotify API used to incorporate music recommendation algorithms.
-        """)
+centered_cols = st.columns([1, 2, 1])
+with centered_cols[1]:
+    st.write("Click the Spotify button to continue...")
+    spotify_url = "https://open.spotify.com/track/3n3Ppam7vgaVa1iaRUc9Lp"
+    if st.button("Listen on Spotify"):
+        st.write(f"Redirecting to Spotify...")
+        st.markdown(f"<a href='{spotify_url}' target='_blank'>Click here if you're not redirected</a>", unsafe_allow_html=True)
+
+
