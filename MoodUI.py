a413baedd0d4b190c8e5c53b31b42c3fb13998a0
natashajyw <wu.tasha88@gmail.com>
2023-10-29 09:33:59 -0700

    briefly linked frontend to backend

--- MoodUI.py
+++ MoodUI.py
@@ -1,8 +1,75 @@
+# MoodUI.py
 import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
 
+# main
+import asyncio
+
+from enum import Enum
+from hume import HumeStreamClient
+from hume.models.config import LanguageConfig
+
 st.set_page_config(page_title="MoodUI", layout="wide")
+
+### Start of main global vars ###
+samples = []
+
+attribute_list = [[0.0 for i in range(4)] for j in range(3)]
+
+class MusAttribute(Enum):
+    ENERGY = 0
+    LIVENESS = 1
+    LOUDNESS = 2
+    TEMPO = 3
+
+### end main global vars ###
+
+###### main stuff #####
+async def CallHume():
+    client = HumeStreamClient("7kBEBKHVRNGmYD4OQfIusARM8g40nvEGf5cXpUiAeohJZhCE")
+    config = LanguageConfig()
+    async with client.connect([config]) as socket:
+        for sample in samples:
+            result = await socket.send_text(sample)
+            emotions = result["language"]["predictions"][0]["emotions"]
+            return emotions
+
+async def DetermineAttributes():
+    list = await CallHume()
+    values_to_keep = ['Anger', 'Anxiety', 'Calmness', 'Distress', 'Ecstasy', 'Enthusiasm', 'Excitement', 'Joy', 'Sadness']
+
+    emotion_list = [item for item in list if item['name'] in values_to_keep]
+
+    emotion_list = sorted(emotion_list, key=lambda x: x['score'], reverse=True)
+    emotion_list = emotion_list[:3]
+
+    for item in emotion_list:
+        print(item)
+
+    for i in range(3):
+        if emotion_list[i]['name'] == 'Anger' or emotion_list[i]['name'] == 'Distress' or emotion_list[i]['name'] == 'Anxiety' or emotion_list[i]['name'] == 'Calmness':
+            attribute_list[i][MusAttribute['ENERGY'].value] = 0.3
+            attribute_list[i][MusAttribute['LIVENESS'].value] = 0.3
+            attribute_list[i][MusAttribute['LOUDNESS'].value] = 0.4
+            attribute_list[i][MusAttribute['TEMPO'].value] = 60
+        elif emotion_list[i]['name'] == 'Ecstasy' or emotion_list[i]['name'] == 'Joy' or emotion_list[i]['name'] == 'Excitement' or emotion_list[i]['name'] == 'Enthusiasm':
+            attribute_list[i][MusAttribute['ENERGY'].value] = 0.8
+            attribute_list[i][MusAttribute['LIVENESS'].value] = 0.8
+            attribute_list[i][MusAttribute['LOUDNESS'].value] = 0.8
+            attribute_list[i][MusAttribute['TEMPO'].value] = 150
+        elif emotion_list[i]['name'] == 'Sadness':
+            attribute_list[i][MusAttribute['ENERGY'].value] = 0.1
+            attribute_list[i][MusAttribute['LIVENESS'].value] = 0.2
+            attribute_list[i][MusAttribute['LOUDNESS'].value] = 0.5
+            attribute_list[i][MusAttribute['TEMPO'].value] = 70
+
+    for item in attribute_list:
+        print(item)
+
+def ReturnMusAttribute():
+    return attribute_list;
+# end main
 
 # function to read the URL for the animation
 def load_lottieurl(url):
@@ -51,10 +118,12 @@ st.write(" ")
 
 centered_cols = st.columns([1, 2, 1])
 with centered_cols[1]:
-    st.write("Click the Spotify button to continue...")
+    st.write("Click on the button below to continue:")
     spotify_url = "https://open.spotify.com/track/3n3Ppam7vgaVa1iaRUc9Lp"
-    if st.button("Listen on Spotify"):
+    if st.button("Make me a Spotify playlist!"):
         st.write(f"Redirecting to Spotify...")
-        st.markdown(f"<a href='{spotify_url}' target='_blank'>Click here if you're not redirected</a>", unsafe_allow_html=True)
+        samples.append(user_input) # adding user input to samples list
+        asyncio.run(DetermineAttributes()) # Run DetermineAttributes withint asyncio
+        st.markdown(f"<a href='{spotify_url}' target='_blank'>Click here if you're not automatically redirected</a>", unsafe_allow_html=True)
 
 
